7136107292b6fd61684ff502811d79f66a9d6e00
kumryung <kumryung@gmail.com>
2019-05-16 20:17:16 +0900

    v0.5.2 업데이트
    모바일 페이지 대응
    class destruct 오류 제거

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-    $http_path = ""; // Showdown Manager 설치되어 있는 디렉토리
-    $client_path = ""; // Showdown 설치되어 있는 디렉토리
+    $http_path = ""; // Showdown Manager 설치되어 있는 디렉토리 ex) /volume1/web/showdown-manager
+    $client_path = ""; // Showdown 설치되어 있는 디렉토리 ex) /volume1/showdown
     $start_page = 2; // 메뉴 번호 1~7
 
     $JAVA_HOME = "/var/packages/Java8/target/j2sdk-image/jre"; // 시스템의 $JAVA_HOME 위치
 
     $showdown_min_ver = 'v1.51'; // Showdown 필요 버전, 수정하지 마세요.
-    $showdown_manager_ver = 'v0.5.1'; // Showdown Manager 버전
+    $showdown_manager_ver = 'v0.5.2'; // Showdown Manager 버전
